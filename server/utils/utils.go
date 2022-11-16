b6e869d4358d8ad955a06777b74e8ab5c74764c4
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2022-11-16 16:57:44 +0530

    [MI-2366]:Fixed review fixes of confluence PR 88 on mattermost

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -1,16 +1,10 @@
 package utils
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"net/url"
-	"path"
 	"regexp"
 	"strings"
 
@@ -101,184 +95,12 @@ func Deduplicate(a []string) []string {
 	return result
 }
 
-type ConfluenceStatus struct {
-	State string `json:"state"`
-}
-
-func NormalizeConfluenceURL(confluenceURL string) (string, error) {
-	u, err := url.Parse(confluenceURL)
-	if err != nil {
-		return "", err
-	}
-	if u.Host == "" {
-		ss := strings.Split(u.Path, "/")
-		if len(ss) > 0 && ss[0] != "" {
-			u.Host = ss[0]
-			u.Path = path.Join(ss[1:]...)
-		}
-		u, err = url.Parse(u.String())
-		if err != nil {
-			return "", err
-		}
-	}
-	if u.Host == "" {
-		return "", errors.Errorf("Invalid URL, no hostname: %q", confluenceURL)
-	}
-	if u.Scheme == "" {
-		u.Scheme = "https"
-	}
-
-	confluenceURL = strings.TrimSuffix(u.String(), "/")
-	return confluenceURL, nil
-}
-
-// CheckConfluenceURL checks if `/status` endpoint of the Confluence URL is accessible
-// and responding with the correct state which is "RUNNING"
-func CheckConfluenceURL(mattermostSiteURL, confluenceURL string, requireHTTPS bool) (_ string, err error) {
-	confluenceURL, err = NormalizeConfluenceURL(confluenceURL)
-	if err != nil {
-		return "", err
-	}
-	if confluenceURL == strings.TrimSuffix(mattermostSiteURL, "/") {
-		return "", errors.Errorf("%s is the Mattermost site URL. Please use your Confluence URL", confluenceURL)
-	}
-
-	defer func() {
-		if err != nil {
-			err = errors.Wrap(err, "we couldn't validate the connection to your Confluence server. "+
-				"This could be because of existing firewall or proxy rules, or because the URL was entered incorrectly")
-		}
-	}()
-
-	resp, err := http.Get(confluenceURL + "/status")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", errors.Errorf("Confluence server returned http status code %q when checking for availability: %q", resp.Status, confluenceURL)
-	}
-
-	resBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var status ConfluenceStatus
-	err = json.Unmarshal(resBody, &status)
-	if err != nil {
-		return "", err
-	}
-	if status.State != "RUNNING" {
-		return "", errors.Errorf("Confluence server is not in correct state, it should be up and running: %q", confluenceURL)
-	}
-	return confluenceURL, nil
-}
-
 func IsConfluenceCloudURL(confluenceURL string) bool {
 	u, err := url.Parse(confluenceURL)
 	if err != nil {
 		return false
 	}
 	return strings.HasSuffix(u.Hostname(), ".atlassian.net")
-}
-
-func CallJSONWithURL(instanceURL, path, method string, in, out interface{}, httpClient *http.Client) (responseData []byte, err error) {
-	urlPath, err := GetEndpointURL(instanceURL, path)
-	if err != nil {
-		return nil, err
-	}
-
-	return CallJSON(instanceURL, method, urlPath, in, out, httpClient)
-}
-
-func GetEndpointURL(instanceURL, path string) (string, error) {
-	endpointURL, err := url.Parse(strings.TrimSpace(fmt.Sprintf("%s%s", instanceURL, path)))
-	if err != nil {
-		return "", err
-	}
-
-	return endpointURL.String(), nil
-}
-
-func CallJSON(url, method, path string, in, out interface{}, httpClient *http.Client) (responseData []byte, err error) {
-	contentType := "application/json"
-	buf := &bytes.Buffer{}
-	err = json.NewEncoder(buf).Encode(in)
-	if err != nil {
-		return nil, err
-	}
-	return call(url, method, path, contentType, buf, out, httpClient)
-}
-
-func call(basePath, method, path, contentType string, inBody io.Reader, out interface{}, httpClient *http.Client) (responseData []byte, err error) {
-	errContext := fmt.Sprintf("confluence: Call failed: method:%s, path:%s", method, path)
-	pathURL, err := url.Parse(path)
-	if err != nil {
-		return nil, errors.WithMessage(err, errContext)
-	}
-
-	if pathURL.Scheme == "" || pathURL.Host == "" {
-		var baseURL *url.URL
-		baseURL, err = url.Parse(basePath)
-		if err != nil {
-			return nil, errors.WithMessage(err, errContext)
-		}
-		if path[0] != '/' {
-			path = "/" + path
-		}
-		path = baseURL.String() + path
-	}
-
-	req, err := http.NewRequest(method, path, inBody)
-	if err != nil {
-		return nil, err
-	}
-	if contentType != "" {
-		req.Header.Add("Content-Type", contentType)
-	}
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.Body == nil {
-		return nil, nil
-	}
-	defer resp.Body.Close()
-
-	responseData, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusOK, http.StatusCreated:
-		if out != nil {
-			err = json.Unmarshal(responseData, out)
-			if err != nil {
-				return responseData, err
-			}
-		}
-		return responseData, nil
-
-	case http.StatusNoContent:
-		return nil, nil
-
-	case http.StatusNotFound:
-		return nil, errors.Errorf(ErrorStatusNotFound)
-	}
-
-	type ErrorResponse struct {
-		Message string `json:"message"`
-	}
-	errResp := ErrorResponse{}
-	err = json.Unmarshal(responseData, &errResp)
-	if err != nil {
-		return nil, err
-	}
-	return responseData, errors.New(errResp.Message)
 }
 
 func GetBodyForExcerpt(htmlBodyValue string) string {
