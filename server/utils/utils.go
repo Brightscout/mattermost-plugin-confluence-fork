757c1e5db4490577335cc6f8f18514e5abaae7d0
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2022-12-21 13:12:39 +0530

    [MI-2483]:Fixed the URL error in config modal

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -256,3 +256,21 @@ func Map(vs []string, f func(string) string) []string {
 	}
 	return vsm
 }
+
+// IsValidURL checks if a given URL is a valid URL with a host and a http or https scheme.
+func IsValidURL(rawURL string) error {
+	u, err := url.ParseRequestURI(rawURL)
+	if err != nil {
+		return err
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return errors.Errorf("URL schema must either be %q or %q", "http", "https")
+	}
+
+	if u.Host == "" {
+		return errors.New("URL must contain a host")
+	}
+
+	return nil
+}
